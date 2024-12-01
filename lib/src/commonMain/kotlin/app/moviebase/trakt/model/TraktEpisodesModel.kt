c387daec7570908f71233c718407d528295ebad2
Christian Krueger <15275971+ChrisKruegerDev@users.noreply.github.com>
2024-12-01 12:07:11 +0100

    Add runtime to episode

--- lib/src/commonMain/kotlin/app/moviebase/trakt/model/TraktEpisodesModel.kt
+++ lib/src/commonMain/kotlin/app/moviebase/trakt/model/TraktEpisodesModel.kt
@@ -4,20 +4,6 @@ import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// TODO: Check if we need both classes
-@Serializable
-data class TraktEpisodeSummary(
-    @SerialName("season") val season: Int,
-    @SerialName("number") val number: Int,
-    @SerialName("title") val title: String,
-    @SerialName("overview") val overview: String? = null,
-    @SerialName("ids") val ids: TraktItemIds? = null,
-    @SerialName("first_aired") val firstAired: Instant? = null,
-    @SerialName("number_abs") val numberAbs: Int? = null,
-    @SerialName("rating") val rating: Int? = null,
-    @SerialName("votes") val votes: Int? = null,
-)
-
 @Serializable
 data class TraktEpisode(
     @SerialName("season") val season: Int,
@@ -29,4 +15,5 @@ data class TraktEpisode(
     @SerialName("first_aired") val firstAired: Instant? = null,
     @SerialName("rating") val rating: Float? = null,
     @SerialName("votes") val votes: Int? = null,
+    @SerialName("runtime") val runtime: Int? = null,
 )
